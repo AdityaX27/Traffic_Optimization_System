701e35bd494d559e6a04a25a77ed5207adb8150b
aditya <100479618+AdityaX27@users.noreply.github.com>
2024-06-26 06:29:20 +0530

    Update floyd2.cpp

--- floyd2.cpp
+++ floyd2.cpp
@@ -1,80 +1,113 @@
 #include <bits/stdc++.h>
-#include <cstdlib>
 using namespace std;
-const int N=1000;
-const int INF=1e9+10;
+const int N = 1000;
+const int INF = 1e9 + 10;
 
 int dist[N][N];
 int td[N][N];
 double ko[N][N];
-int dis[N];
 int net[N][N];
 
+void initializeMatrices() {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (i == j) {
+                dist[i][j] = 0;
+                td[i][j] = 0;
+                ko[i][j] = 0;
+            } else {
+                dist[i][j] = INF;
+                td[i][j] = INF;
+                ko[i][j] = INF;
+            }
+            net[i][j] = -1; // Initialize net with -1 to indicate no path
+        }
+    }
+}
 
-int main()
-{
-    for(int i=0;i<N;++i)
-    {
-        for(int j=0;j<N;++j)
-        {
-            if(i==j){dist[i][j]=0;td[i][j]==0;ko[i][j]=0;}
-            else {dist[i][j]=INF;td[i][j]=INF;ko[i][j]=INF;}
+void floydWarshall(int n) {
+    for (int k = 1; k <= n; ++k) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (ko[i][j] > ko[i][k] + ko[k][j]) {
+                    ko[i][j] = ko[i][k] + ko[k][j];
+                    net[i][j] = net[i][k];
+                }
+            }
         }
     }
-    int n,m; //n vertices m no of edges
-    cin >> n >>m;
-    for(int i=0;i<m;++i)
-    {
-        int x,y,wt,kt;// inputting weights and no of vehicals between two nodes
-        cin>>x>>y>>wt>>kt; 
-        dist[x][y]=wt;
-        td[x][y]=kt;
-        ko[x][y]=(wt/(60-(0.05*kt)));//optimization equation
-     
-    }
-   // net[i][j] stores vertex next to i in the shortest path between (i,j)
-      for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-          if(ko[i][j]==0)ko[i][j]=INF;  // there is no edge b/w (i,j)
-          else net[i][j]=j;    // if there is an edge b/w i and j then j should be next to i
-        }
-      }
+}
 
-      for(int k=1;k<=n;k++){
-        for(int i=1;i<=n;i++){
-          for(int j=1;j<=n;j++){
-            if(ko[i][j]>ko[i][k]+ko[k][j]){
-              ko[i][j]=ko[i][k]+ko[k][j];
-              net[i][j]=net[i][k];  // we found a vertx k next to i which further decrease the shortest path b/w (i,j) so updated it
-            }
-          }
-        }
-      }
-      cout<<"-----OPTIMIZED SHORTEST TIME MATRIX(hrs)----"<<endl;
-    cout<<endl;
-    for(int i=1;i<=n;++i)
-        {
-            for(int j=1;j<=n;++j)
-            { if(ko[i][j]==INF)
-            {
-                cout<<"I"<<" ";
-            }
-            else{
-               cout<< ko[i][j]<<" ";
+void printMatrix(int n) {
+    cout << "-----OPTIMIZED SHORTEST TIME MATRIX(hrs)----" << endl << endl;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (ko[i][j] == INF) {
+                cout << "I ";
+            } else {
+                cout << ko[i][j] << " ";
             }
         }
-        cout<<endl;
+        cout << endl;
     }
-      // for printing the shortest optimized path between any two nodes in the graph given by the user
-      int u,v;
-      cout<<"Enter starting and ending node respectively to find the shortest path"<<endl;
-      cin>>u>>v;
-         
-          cout<<u;
-          while(u!=v){
-            u=net[u][v];
-            cout<<"-->"<<u;
-          }
-          cout<<endl;
-       
+}
+
+void printPath(int u, int v) {
+    if (net[u][v] == -1) {
+        cout << "No path exists between " << u << " and " << v << endl;
+        return;
+    }
+    cout << u;
+    while (u != v) {
+        u = net[u][v];
+        cout << " --> " << u;
+    }
+    cout << endl;
+}
+
+int main() {
+    initializeMatrices();
+
+    int n, m; // n vertices, m number of edges
+    cout << "Enter number of vertices and edges: ";
+    cin >> n >> m;
+
+    cout << "Enter edges (x, y, weight, traffic density):" << endl;
+    for (int i = 0; i < m; ++i) {
+        int x, y, wt, kt;
+        cin >> x >> y >> wt >> kt;
+
+        if (x <= 0 || x > n || y <= 0 || y > n || x == y) {
+            cout << "Invalid input for edge. Please enter valid nodes." << endl;
+            continue;
+        }
+
+        dist[x][y] = wt;
+        td[x][y] = kt;
+        ko[x][y] = (wt / (60 - (0.05 * kt)));
+        net[x][y] = y;
+    }
+
+    // Running the Floyd-Warshall algorithm
+    floydWarshall(n);
+
+    // Printing the optimized shortest time matrix
+    printMatrix(n);
+
+    char choice;
+    do {
+        int u, v;
+        cout << "Enter starting and ending node respectively to find the shortest path: ";
+        cin >> u >> v;
+        if (u <= 0 || u > n || v <= 0 || v > n) {
+            cout << "Invalid nodes. Please enter valid nodes." << endl;
+        } else {
+            printPath(u, v);
+        }
+
+        cout << "Do you want to find another path? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
 }
